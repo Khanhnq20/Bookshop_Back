9559e1fdffd1d7961902755bfe1b6215b2449e9b
OS <OS@DESKTOP-NLC0EVM>
2022-06-30 17:49:54 +0700

    Add project files.

---
+++ Bookshop/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Bookshop.DTOs;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Bookshop.Controllers
+{
+    [ApiController]
+    [Route("api/accounts")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateUser([FromBody] UserInfo model)
+        {
+            var user = new IdentityUser { UserName = model.EmailAddress, Email = model.EmailAddress };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok();
+        }
+        private UserToken BuildToken(UserInfo userInfo)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, userInfo.EmailAddress),
+                new Claim(ClaimTypes.Email, userInfo.EmailAddress),
+                new Claim("mykey","whatever value I want")
+            };
+
+            var key = new SymmetricSecurityKey(E)
+        }
+        
+    }
+}
---
+++ Bookshop/DTOs/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookshop.DTOs
+{
+    public class UserInfo
+    {
+        [EmailAddress]
+        [Required]
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
